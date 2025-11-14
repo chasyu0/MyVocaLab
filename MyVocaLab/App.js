9e2f36c7c4b613ce03ccec9a1eccf09447848ee2
crazyapoll0 <happyjude01@gmail.com>
2025-11-14 15:08:21 +0900

    벨로그작성용환경세팅파일예시파일두개만듬.
    .

---
+++ MyVocaLab/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>안녕하세요. 이제막 공부하는중인데 파판하고 싶네요 ㅠㅠ</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
